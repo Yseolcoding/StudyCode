f8a646045ed137da5322648e0337193a2622c8bd
Yuni <yseolcoding@gmail.com>
2024-01-11 10:11:05 +0900

    review ex

---
+++ springQ/BoardWeb/src/main/java/polymorphism/review/LgTV.java
@@ -0,0 +1,21 @@
+package polymorphism.review;
+
+public class LgTV {
+	
+	public void turnOn() {
+		System.out.println("LgTV---전원 켠다.");
+	}
+	
+	public void turnOff() {
+		System.out.println("LgTV---전원 켠다.");
+	}
+	
+	public void soundUp() {
+		System.out.println("LgTV---전원 켠다.");
+	}
+	
+	public void soundDown() {
+		System.out.println("LgTV---전원 켠다.");
+	}
+
+}
---
+++ springQ/BoardWeb/src/main/java/polymorphism/review/TVUser.java
@@ -0,0 +1,14 @@
+package polymorphism.review;
+
+public class TVUser {
+	public static void main(String[] args) {
+
+	SamsungTV tv = new SamsungTV();
+	
+	tv.powerOn();
+	tv.volumeUp();
+	tv.volumeDown();
+	tv.powerOff();
+	
+	}
+}
