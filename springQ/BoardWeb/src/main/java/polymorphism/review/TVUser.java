0bd58e7fd2efd6068f648881dd20a641a50acee0
Yuni <yseolcoding@gmail.com>
2024-01-11 13:37:09 +0900

    ex singleton

--- springQ/BoardWeb/src/main/java/polymorphism/review/TVUser.java
+++ springQ/BoardWeb/src/main/java/polymorphism/review/TVUser.java
@@ -11,10 +11,9 @@ public class TVUser {
 	
 		// 2. Spring 컨테이너로부터 필요한 객체를 요청(LookUp)한다.
 		TV tv = (TV)factory.getBean("tv");
-		tv.powerOn();
-		tv.powerOff();
-		tv.volumeUp();
-		tv.volumeDown();
+		TV tv2 = (TV)factory.getBean("tv");
+		TV tv3 = (TV)factory.getBean("tv");
+		
 		
 		// 3. Spring 컨테이너를 종료한다.
 		factory.close();
