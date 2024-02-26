e1df6abc9d3ba5a8426613055269b2f96913647c
Yuni <yseolcoding@gmail.com>
2024-02-26 09:22:34 +0900

    Modify DispatcherServlet

--- springQ/BoardWebM1/src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ springQ/BoardWebM1/src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -19,14 +19,23 @@ import com.springbook.biz.user.impl.UserDAO;
  */
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private HandlerMapping handlerMapping;
+	private ViewResolver viewResolver;
+
+	public void init() throws ServletException {
+		handlerMapping = new HandlerMapping();
+		viewResolver = new ViewResolver();
+		viewResolver.setPrefix("./");
+		viewResolver.setSuffix(".jsp");
+	}
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DispatcherServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+//    /**
+//     * @see HttpServlet#HttpServlet()
+//     */
+//    public DispatcherServlet() {
+//        super();
+//        // TODO Auto-generated constructor stub
+//    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -52,7 +61,26 @@ public class DispatcherServlet extends HttpServlet {
 		String path = uri.substring(uri.lastIndexOf("/"));
 		System.out.println(path);
 		
-		// 2. 클라이언트의 요청 path에 따라 적절히 분기처리 한다.
+		// 2. HandlerMapping을 통해 path에 해당하는 Controller를 검색한다.
+		Controller ctrl = handlerMapping.getController(path);
+		
+		// 3. 검색된 Controller를 실행한다.
+		String viewName = ctrl.handleRequest(request, response);
+		
+		// 4. ViewResolver를 통해 viewName에 해당하는 화면을 검색한다.
+		String view = null;
+		if(!viewName.contains(".do")) {
+			view = viewResolver.getView(viewName);
+		} else {
+			view = viewName;
+		}
+		
+		// 5. 검색된 화면으로 이동한다.
+		response.sendRedirect(view);
+		
+		
+		
+/*		// 2. 클라이언트의 요청 path에 따라 적절히 분기처리 한다.
 		if(path.equals("/login.do")) {
 //			System.out.println("로그인 처리");
 //			
@@ -170,6 +198,7 @@ public class DispatcherServlet extends HttpServlet {
 			response.sendRedirect("login.jsp");
 			
 		} 
+		*/
 	}
 
 }
