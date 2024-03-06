a5401ccacf42dd768156fc39d45a26e449360158
Yuni <yseolcoding@gmail.com>
2024-03-06 14:19:13 +0900

    chapter 1, ex 1

--- HFJavaPr/.idea/sonarlint/securityhotspotstore/index.pb
+++ HFJavaPr/.idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,9 @@
 
 =
 src/Main.java,9/2/924f089edb4af5ee45c8a9d1129cd28e2b368138
+C
+src/BottleSong.java,8/f/8f3c63c31a908fe1fe226b82d4b5e6e4c5eb744d
+S
+#src/com/example/ch1/BottleSong.java,2/6/26beaca5232cfae85f6192cd54292277cd8fb6bf
+X
+(src/main/com/example/ch1/BottleSong.java,c/b/cba340e19707a975379191c3004fcdb7de7a5b66
---
+++ HFJavaPr/src/main/com/example/ch1/BottleSong.java
@@ -0,0 +1,26 @@
+package main.com.example.ch1;
+
+public class BottleSong {
+    public static void main(String[] args){
+        int bottlesNum = 10;
+        String word = "bottles"; // 복수형
+
+        while(bottlesNum > 0) {
+            if (bottlesNum == 1) {
+                word = "bottle"; // 단수형 (한 병인 경우)
+            }
+
+                System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
+                System.out.println(bottlesNum + " green " + word + ", hanging on the wall");
+                System.out.println("And if one green bottle should accidentally fall,");
+                bottlesNum = bottlesNum - 1;
+
+                if (bottlesNum > 0) {
+                    System.out.println("There'll be " + bottlesNum +
+                            " green " + word + ", hanging on the wall");
+                } else {
+                    System.out.println("There'll be no green bottles, hanging on the wall. ");
+                } // else 끝
+        }
+    }
+}
