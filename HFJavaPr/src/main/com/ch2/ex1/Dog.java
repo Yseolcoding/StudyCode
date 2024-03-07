dbec57efdf8763d75d277945128908af8f1c4cb2
Yuni <yseolcoding@gmail.com>
2024-03-07 09:39:00 +0900

    ch02 ex01

--- HFJavaPr/src/main/com/ch2/ex1/Dog.java
+++ HFJavaPr/src/main/com/ch2/ex1/Dog.java
@@ -2,10 +2,11 @@ package main.com.ch2.ex1;
 
 
 public class Dog {
+    // 인스턴스 변수
     int size;
     String breed;
     String name;
-
+    //메서드
     void bark() {
         System.out.println("Ruff! Ruff!");
     }
--- HFJavaPr/src/main/com/ch2/ex1/DogTestDrive.java
+++ HFJavaPr/src/main/com/ch2/ex1/DogTestDrive.java
@@ -5,8 +5,12 @@ package main.com.ch2.ex1;
  */
 public class DogTestDrive {
     public static void main(String[] args) {
+
+        // Dog 객체 생성
         Dog d = new Dog();
+        // 점 연산자(.)를 써서 Dog의 크기를 설정합니다.
         d.size = 40;
+        // bark() 메서드 호출
         d.bark();
     }
 }
