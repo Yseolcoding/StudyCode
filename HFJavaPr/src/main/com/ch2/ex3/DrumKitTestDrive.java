3c8e05a3712b2833cbd46730daaa30f6129a7e98
Yuni <yseolcoding@gmail.com>
2024-03-07 10:55:29 +0900

    ch2 fixed ex3

--- HFJavaPr/src/main/com/ch2/ex3/DrumKitTestDrive.java
+++ HFJavaPr/src/main/com/ch2/ex3/DrumKitTestDrive.java
@@ -6,12 +6,13 @@ public class DrumKitTestDrive {
     public static void main(String[] args) {
 
     DrumKit d = new DrumKit();
+    d.playSnare();
+    d.snare=false;
+    d.playTopHat();
 
     if(d.snare == true){
         d.playSnare();
     }
-    d.snare = false;
-    d.playTopHat();
 
     }
 }
