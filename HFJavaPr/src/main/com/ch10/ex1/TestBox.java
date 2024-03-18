ed89e3152fdc0611b599ac3ff96ba951b6cb1895
Yuni <yseolcoding@gmail.com>
2024-03-18 09:02:52 +0900

    ch10 ex1, Add Comment

--- HFJavaPr/src/main/com/ch10/ex1/TestBox.java
+++ HFJavaPr/src/main/com/ch10/ex1/TestBox.java
@@ -1,5 +1,8 @@
 package main.com.ch10.ex1;
 
+/*
+    p.335 쓰면서 제대로 공부하기 예시
+ */
 public class TestBox {
     private Integer i;
     private int j;
@@ -15,3 +18,7 @@ public class TestBox {
         System.out.println(i);
     }
 }
+
+/*
+    결과 : Integer 값이 null일 경우 int에 대입되지 않고 에러가 발생한다.
+ */
