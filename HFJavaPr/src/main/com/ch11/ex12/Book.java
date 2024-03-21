bb24b83c1313627608d794c3a012a7832947443b
Yuni <yseolcoding@gmail.com>
2024-03-21 11:07:31 +0900

    ch11 ex12

---
+++ HFJavaPr/src/main/com/ch11/ex12/Book.java
@@ -0,0 +1,8 @@
+package main.com.ch11.ex12;
+
+public class Book {
+    private String title;
+    public Book(String t){
+        title = t;
+    }
+}
--- HFJavaPr/src/main/com/ch11/ex12/TestTree.java
+++ HFJavaPr/src/main/com/ch11/ex12/TestTree.java
@@ -1,4 +1,31 @@
 package main.com.ch11.ex12;
 
+import java.util.Set;
+import java.util.TreeSet;
+
+/*
+    p.395 TreeSet에 관해 꼭 알아야 할 것들
+ */
 public class TestTree {
+    public static void main(String[] args) {
+        new TestTree().go();
+    }
+
+    public void go() {
+        Book b1 = new Book("How Cats Work");
+        Book b2 = new Book("Remix your body");
+        Book b3 = new Book("Finding Emo");
+
+        Set<Book> tree = new TreeSet<>();
+        tree.add(b1);
+        tree.add(b2);
+        tree.add(b3);
+        System.out.println(tree);
+    }
 }
+
+/*
+    1. 이 코드를 컴파일하면 어떻게 될까?
+    - 컴파일은 정상적으로 실행된다.
+    2.
+ */
