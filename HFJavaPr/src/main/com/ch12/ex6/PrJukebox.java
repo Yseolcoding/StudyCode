3398e4372cce8f62d01cb2b1f3ede7e932e0b78c
Yuni <yseolcoding@gmail.com>
2024-03-25 08:39:53 +0900

    test something

--- HFJavaPr/src/main/com/ch12/ex6/PrJukebox.java
+++ HFJavaPr/src/main/com/ch12/ex6/PrJukebox.java
@@ -1,5 +1,9 @@
 package main.com.ch12.ex6;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 /*
     p. 441 모의코드를 사용한 전체 출력 메인 메소드 구현 (람다 사용)
  */
@@ -7,5 +11,9 @@ public class PrJukebox {
     public static void main(String[] args) {
         Songs songs = new Songs();
         songs.getSongs().forEach(song -> System.out.println(song));
+
+        List<String> list = Arrays.asList("Apple", "Banana", "Cherry");
+        //Consumer<String> printConsumer = s -> System.out.println(s);
+        list.forEach(s -> System.out.println(s));
     }
 }
