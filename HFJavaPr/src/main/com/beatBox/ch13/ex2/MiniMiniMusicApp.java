746f376232f83beac2dd6508c1bde3241fc4a75c
Yuni <yseolcoding@gmail.com>
2024-03-26 09:13:43 +0900

    ch13 ex2: Add Comments

--- HFJavaPr/src/main/com/beatBox/ch13/ex2/MiniMiniMusicApp.java
+++ HFJavaPr/src/main/com/beatBox/ch13/ex2/MiniMiniMusicApp.java
@@ -27,9 +27,10 @@ public class MiniMiniMusicApp {
             // 4. Track에 MidiEvent를 집어넣습니다. 이 부분은 거의 인스턴스 코드라고 보면 됩니다.
             // 중요한 부분은 setMessage() 메서드에 대한 인자와 MidiEvent 생성자에 대한 인자입니다.
             ShortMessage msg1 = new ShortMessage();
-            msg1.setMessage(NOTE_ON, 1, 44, 100);
-            MidiEvent noteOn = new MidiEvent(msg1, 1);
-            track.add(noteOn);
+            msg1.setMessage(NOTE_ON, 1, 44, 100);                   // "44번 음표를 연주하는 것을 시작해라"
+            MidiEvent noteOn = new MidiEvent(msg1, 1);                              // 지시사항은 메시지에 들어 있지만, MidiEvent에서는 그 지시사항을 가동할 시기를 추가해줍니다. 이 MidiEvent에서는 'a'라는 메시지를 첫번째 박자(1번 비트)에서 가동시키도록 지정했습니다.
+            track.add(noteOn);                                                           // Track에 MidiEvent 객체가 저장됩니다. Sequence에서는 각 이벤트가 일어나야할 시기를 바탕으로 Track을 정리하고 Sequencer에서는 그렇게 주어진 순서에 따라 음악을 재생합니다.
+                                                                                         // 정확하게 똑같은 시각에 이벤트 여러개가 동시에 일어날 수도 있습니다. 예를 들어서, 음표 두 개를 동시에 연주하거나 서로 다른 악기를 동시에 연주할 수 있습니다.
 
             ShortMessage msg2 = new ShortMessage();
             msg2.setMessage(NOTE_OFF, 1, 44, 100);
@@ -46,3 +47,11 @@ public class MiniMiniMusicApp {
         }
     }
 }
+
+/*
+    setMessage의 첫번째 인자는 언제나 메시지 '유형'을 나타내는 값, 나머지 인자 세 개의 의미는 메시지 유형에 따라 바뀜
+    NOTE_ON, NOTE_OF (연주시작[144], 연주종료[128])
+    Channel : 어떤 악기? (밴드에 있는 연주자라고 생각, 1번 채널은 1번 키보드 연주자, 9번 채널은 드러머처럼)
+    data1 : 음 (0 ~ 127, 숫자가 높을수록 높은 음)
+    data2 : 건반을 누르는 속도 (0은 가장 부드럽게 누르는 것으로 거의 소리가 들리지 않고, 보통 기본값으로 100 정도를 씁니다.)
+ */
