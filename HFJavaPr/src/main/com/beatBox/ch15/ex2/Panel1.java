ddcea46e2da03b423118e8e97ddab909a604651b
Yuni <yseolcoding@gmail.com>
2024-04-02 17:26:45 +0900

    Modify: ch15 ex2

--- HFJavaPr/src/main/com/beatBox/ch15/ex2/Panel1.java
+++ HFJavaPr/src/main/com/beatBox/ch15/ex2/Panel1.java
@@ -16,9 +16,19 @@ public class Panel1 {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
         panel.setBackground(Color.blue);
+
+        JButton button = new JButton("shock me");
+        panel.add(button);
+
         frame.getContentPane()
              .add(BorderLayout.EAST, panel);
         frame.setSize(200, 200);
         frame.setVisible(true);
     }
 }
+
+/*
+    패널의 레이아웃 관리자(FlowLayout 관리자)가 버튼을 제어하고 프레임의 레이아웃 관리자(BorderLayout 관리자)가 패널을 관리합니다.
+
+    패널이 확장되었습니다. 그리고 버튼은 너비와 높이가 모두 버튼이 원하는 크기로 맞춰집니다.
+ */
